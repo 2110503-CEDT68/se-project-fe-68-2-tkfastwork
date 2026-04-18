81d293f66022958800c08e4b2c5aba2e96fa8577
AvenaSat1va <oatsaeow@gmail.com>
2026-04-18 23:50:09 +0700

    Separate front end from backend

--- routes/recommend.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getRecommendation } = require('../controllers/recommend');
-const { protect } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.post('/', protect, getRecommendation);
-
-module.exports = router;
